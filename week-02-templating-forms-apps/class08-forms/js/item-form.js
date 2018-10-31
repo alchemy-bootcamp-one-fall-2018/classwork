e0eabd99a757a044b682cc4e75a5201c3c2c53d0
martypdx <marty@martypdx.com>
2018-10-31 08:46:22 -0700

    comments on form submit

--- week-02-templating-forms-apps/class08-forms/js/item-form.js
+++ week-02-templating-forms-apps/class08-forms/js/item-form.js
@@ -3,11 +3,22 @@ const form = document.getElementById('form');
 
 const itemForm = {
     init(onAdd) {
-        form.addEventListener('submit', function() {
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
 
-            // gather data
+            // form elements collection
+            const elements = form.elements;
 
-            onAdd(/*data*/);
+            // data object
+            const item = {};
+            
+            // gather data, ie:
+            item.title = elements.title.value;
+
+
+
+            // call the callback with new item
+            onAdd(item);
         });
 
     }
