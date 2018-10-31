78426d5390a30dffbf36fc825fa8d411a973dda1
martypdx <marty@martypdx.com>
2018-10-31 12:19:26 -0700

    save data to local storage

--- week-02-templating-forms-apps/class08-forms/demo2/js/item-form.js
+++ week-02-templating-forms-apps/class08-forms/demo2/js/item-form.js
@@ -1,8 +1,18 @@
 
-const form = document.getElementById('form');
+const form = document.getElementById('mafia-application');
+const spaghettiRange = document.getElementById('spaghetti');
+const spaghettiDisplay = document.getElementById('spaghetti-display');
+const howMany = document.getElementById('how-many');
 
 const itemForm = {
     init(onAdd) {
+        
+        function setSpaghettiDisplay() {
+            spaghettiDisplay.textContent = spaghettiRange.value;
+        }
+        setSpaghettiDisplay();
+        spaghettiRange.addEventListener('change', setSpaghettiDisplay);
+
         form.addEventListener('submit', function(event) {
             event.preventDefault();
 
@@ -10,15 +20,45 @@ const itemForm = {
             const elements = form.elements;
 
             // data object
-            const item = {};
+            const mafiosx = {};
             
             // gather data, ie:
-            item.title = elements.title.value;
+            mafiosx.name = elements.name.value;
 
+            howMany.style.background = '';
+            mafiosx.murder = elements.murder.value === 'yes';
+            if(mafiosx.murder) {
+                const count = parseInt(elements.kills.value);
+                if(!isNaN(count)) {
+                    mafiosx.kills = count;
+                }
+                else {
+                    // set an error message
+                    howMany.style.background = 'red';
+                    return;
+                }
+            }
 
+            mafiosx.weapons = [];
+
+            for(let i = 0; i < elements.weapons.length; i++) {
+                const weapon = elements.weapons[i];
+                if(weapon.checked) {
+                    mafiosx.weapons.push(weapon.value);
+                }
+            }
+
+            mafiosx.role = elements.role.value;
+
+            mafiosx.awesome = parseInt(elements.spaghetti.value);
 
             // call the callback with new item
-            onAdd(item);
+            onAdd(mafiosx);
+
+            // use an onscreen message
+            alert('thanks for your interest');
+            form.reset();
+
         });
 
     }
